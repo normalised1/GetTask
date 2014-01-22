c937e5c29812a348b41a3a0cba335320ae3b37ac
normalised <normalised@gmail.com>
2014-01-22 16:05:25 +0000

    Added style.

--- DailyTask.py
+++ DailyTask.py
@@ -6,13 +6,27 @@ from task.task import GetTask
 
 
 def application(environ, start_response):
-	date = datetime.datetime.now()
+	previous = False
+	if str(environ['QUERY_STRING'])=='previous':
+		date = datetime.datetime.now() - datetime.timedelta(days=1)
+		previous = True
+	else:
+		date = datetime.datetime.now()
+
 	day = date.day + 208 
 	month = date.month
 	year = date.year
 	seed = day * 100 + month * 1000 + year
-	output = '''<html><head></head>
-<body>Daily task for <b>''' + datetime.date.today().strftime("%B %d, %Y") + '</b><h2>' + str(GetTask(seed)) + '</h2></body></html>'
+
+	output = '''<html><head><link rel="stylesheet" type="text/css" href="style.css"></head>
+<body>Daily task for <b>''' + date.strftime("%B %d, %Y") + '</b><h2>' + str(GetTask(seed)) + '</h2>'
+
+	if previous != True:
+		output += '<a href="daily_task?previous">yesterday</a>'
+	else:
+		output += '<a href="daily_task">today</a>'
+
+	output += '</body></html>'
 
 	response_headers = [
 		('Content-Length', str(len(output))),
