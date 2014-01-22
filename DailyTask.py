1c05ad899737ccdfe3b3b37121c8676af8f99fb9
normalised <normalised@gmail.com>
2014-01-22 00:53:51 +0000

    Added pretty formatting.

--- DailyTask.py
+++ DailyTask.py
@@ -11,11 +11,12 @@ def application(environ, start_response):
 	month = date.month
 	year = date.year
 	seed = day * 100 + month * 1000 + year
-	output = str(GetTask(seed))
+	output = '''<html><head></head>
+<body>Daily task for <b>''' + datetime.date.today().strftime("%B %d, %Y") + '</b><h2>' + str(GetTask(seed)) + '</h2></body></html>'
 
 	response_headers = [
 		('Content-Length', str(len(output))),
-		('Content-Type', 'text/plain'),
+		('Content-Type', 'text/html'),
 	]
 
 	start_response('200 OK', response_headers)
