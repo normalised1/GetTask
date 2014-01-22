4b79abb92d3f8584b5a0b63603b5f57d5a390b6a
normalised <normalised@gmail.com>
2014-01-22 18:34:40 +0000

    Added html to random task.

--- RandomTask.py
+++ RandomTask.py
@@ -7,11 +7,11 @@ from task.task import GetTask
 
 def application(environ, start_response):
 	date = datetime.datetime.now()
-	output = str(GetTask(date.microsecond))
+	output = '<html><head><link rel="stylesheet" type="text/css" href="style.css"></head><body><br><p><b>Random</b> task:<h2>' + str(GetTask(date.microsecond))+ '</h2></p></body></html>'
 
 	response_headers = [
 		('Content-Length', str(len(output))),
-		('Content-Type', 'text/plain'),
+		('Content-Type', 'text/html'),
 	]
 
 	start_response('200 OK', response_headers)
